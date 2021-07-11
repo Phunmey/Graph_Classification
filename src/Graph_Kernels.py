885aaf8146e509eb6c9d6890d524f82d916967b3
speters-github <umpet347@myumanitoba.ca>
2021-07-11 14:55:48 -0500

    Renamed functions and variables to fit python's standards (lowercase and underscores).

--- src/Graph_Kernels.py
+++ src/Graph_Kernels.py
@@ -14,85 +14,85 @@ from time import time
 from grakel.kernels import WeisfeilerLehman, VertexHistogram, WeisfeilerLehmanOptimalAssignment, ShortestPath
 
 
-def standardGraphFile(dataset):
+def standard_graph_file(data_set):
     start = time()
-    DATA, graphlabels_aslist = get_data_and_labels(dataset)
-    perform_rfc(DATA, graphlabels_aslist, start)
+    data, graph_labels = get_data_and_labels(data_set)
+    perform_rfc(data, graph_labels, start)
 
 
-def get_data_and_labels(dataset):
-    edges_asdf = get_read_csv(dataset, "_A.txt")
-    edges_asdf.columns = ['from', 'to']
-    unique_nodes = ((edges_asdf['from'].append(edges_asdf['to'])).unique()).tolist()
+def get_data_and_labels(data_set):
+    edges = get_read_csv(data_set, "_A.txt")
+    edges.columns = ['from', 'to']
+    unique_nodes = ((edges['from'].append(edges['to'])).unique()).tolist()
     missing_nodes = [x for x in range(unique_nodes[0], unique_nodes[-1] + 1) if
                      x not in unique_nodes]  # find the missing nodes
-    node_list = unique_nodes + missing_nodes
-    node_list.sort()
-    graphindicator_aslist = get_csv_value_sum(dataset, "_graph_indicator.txt")
-    graphlabels_aslist = get_csv_value_sum(dataset, "_graph_labels.txt")
-    nodelabels_aslist = get_csv_value_sum(dataset, "_node_labels.txt")
-    nodes_dict = dict(zip(node_list, nodelabels_aslist))
-    unique_graphindicator = list(set(graphindicator_aslist))
+    nodes = unique_nodes + missing_nodes
+    nodes.sort()
+    graph_indicators = get_csv_value_sum(data_set, "_graph_indicator.txt")
+    graph_labels = get_csv_value_sum(data_set, "_graph_labels.txt")
+    node_labels = get_csv_value_sum(data_set, "_node_labels.txt")
+    nodes_dict = dict(zip(nodes, node_labels))
+    unique_graph_indicator = list(set(graph_indicators))
 
-    DATA = []
-    for i in unique_graphindicator:
-        graphid = i
-        graphid_loc = [index for index, element in enumerate(graphindicator_aslist)
-                       if element == graphid]
-        edges_loc = edges_asdf[edges_asdf.index.isin(graphid_loc)]
+    data = []
+    for i in unique_graph_indicator:
+        graph_id = i
+        graph_id_loc = [index for index, element in enumerate(graph_indicators)
+                       if element == graph_id]
+        edges_loc = edges[edges.index.isin(graph_id_loc)]
         edges_loc_asset = (edges_loc.to_records(index=False)).tolist()
-        nodes_aslist = ((edges_loc['from'].append(edges_loc['to'])).unique()).tolist()
-        ext = {k: nodes_dict[k] for k in nodes_aslist if k in nodes_dict}
+        node_list = ((edges_loc['from'].append(edges_loc['to'])).unique()).tolist()
+        ext = {k: nodes_dict[k] for k in node_list if k in nodes_dict}
         edges_nodes = [edges_loc_asset, ext]
-        DATA.append(edges_nodes)
+        data.append(edges_nodes)
 
-    return DATA, graphlabels_aslist
+    return data, graph_labels
 
 
-def get_read_csv(dataset, extension):
+def get_read_csv(data_set, extension):
     path = "../data"
-    return pd.read_csv(path + "/" + dataset + "/" + dataset + extension, header=None)
+    return pd.read_csv(path + "/" + data_set + "/" + data_set + extension, header=None)
 
 
-def get_csv_value_sum(dataset, extension):
-    return sum((get_read_csv(dataset, extension).values.tolist()), [])
+def get_csv_value_sum(data_set, extension):
+    return sum((get_read_csv(data_set, extension).values.tolist()), [])
 
 
-def perform_rfc(DATA, graphlabels_aslist, start):
-    G_train, G_test, y_train, y_test = train_test_split(DATA, graphlabels_aslist,
+def perform_rfc(data, graph_labels, start):
+    g_train, g_test, y_train, y_test = train_test_split(data, graph_labels,
                                                         test_size=0.2, random_state=42)
 
-    WL = WeisfeilerLehman(n_iter=4, base_graph_kernel=VertexHistogram, normalize=True)
-    K_train = WL.fit_transform(G_train)
-    K_test = WL.transform(G_test)
+    w_lehman = WeisfeilerLehman(n_iter=4, base_graph_kernel=VertexHistogram, normalize=True)
+    k_train = w_lehman.fit_transform(g_train)
+    k_test = w_lehman.transform(g_test)
 
-    RFC_pred = RandomForestClassifier().fit(K_train, y_train)
-    y_pred = RFC_pred.predict(K_test)  # predict the class for K_test
+    rfc_pred = RandomForestClassifier().fit(k_train, y_train)
+    y_pred = rfc_pred.predict(k_test)  # predict the class for k_test
 
     # PREDICTION PROBABILITIES
-    r_probs = [0 for _ in range(len(y_test))]  # worst case scenario
-    RFC_probs = (RFC_pred.predict_proba(K_test))[:,
-                1]  # predict the class prob. for K_test and keep the positive outcomes
-    r_auc = roc_auc_score(y_test, r_probs)
-    RFC_auc = roc_auc_score(y_test, RFC_probs)  # Compute AUROC scores
+    r_prob = [0 for _ in range(len(y_test))]  # worst case scenario
+    rfc_prob = (rfc_pred.predict_proba(k_test))[:,
+                1]  # predict the class prob. for k_test and keep the positive outcomes
+    r_auc = roc_auc_score(y_test, r_prob)
+    rfc_auc = roc_auc_score(y_test, rfc_prob)  # Compute AUROC scores
 
-    print('Random prediction: AUROC = %.3f' % (r_auc))
-    print('RFC: AUROC = %.3f' % (RFC_auc))
+    print('Random prediction: AUROC = %.3f' % r_auc)
+    print('RFC: AUROC = %.3f' % rfc_auc)
     print(accuracy_score(y_test, y_pred))
     print(f'Time taken to run:{time() - start} seconds')
 
-    plot_roc_curve(y_test, r_probs, RFC_probs, r_auc, RFC_auc)
+    plot_roc_curve(y_test, r_prob, rfc_prob, r_auc, rfc_auc)
 
 
-def plot_roc_curve(y_test, r_probs, RFC_probs, r_auc, RFC_auc):
+def plot_roc_curve(y_test, r_prob, rfc_prob, r_auc, rfc_auc):
     # PLOTTING THE ROC_CURVE
-    r_fpr, r_tpr, thresholds = roc_curve(y_test, r_probs, pos_label=2)
-    RFC_fpr, RFC_tpr, thresholds = roc_curve(y_test, RFC_probs, pos_label=2)  # compute ROC
-    # RFAC_auc = auc(RFC_fpr, RFC_tpr)
+    r_fpr, r_tpr, thresholds = roc_curve(y_test, r_prob, pos_label=2)
+    rfc_fpr, RFC_tpr, thresholds = roc_curve(y_test, rfc_prob, pos_label=2)  # compute ROC
+    # rfac_auc = auc(rfc_fpr, RFC_tpr)
 
     plt.figure(figsize=(4, 4), dpi=100)
     plt.plot(r_fpr, r_tpr, marker='.', label='Chance prediction (AUROC= %.3f)' % r_auc)
-    plt.plot(RFC_fpr, RFC_tpr, linestyle='-', label='RFC (AUROC= %.3f)' % RFC_auc)
+    plt.plot(rfc_fpr, RFC_tpr, linestyle='-', label='RFC (AUROC= %.3f)' % rfc_auc)
     plt.title('ROC Plot')  # title
     plt.xlabel('False Positive Rate')  # x-axis label
     plt.ylabel('True Positive Rate')  # y-axis label
@@ -102,7 +102,7 @@ def plot_roc_curve(y_test, r_probs, RFC_probs, r_auc, RFC_auc):
 
 if __name__ == '__main__':
     dataset = 'PROTEINS'
-    standardGraphFile(dataset)
+    standard_graph_file(dataset)
 
     # TO DO:
     # GridsearchCV documentation and output tab separated files for Emma
