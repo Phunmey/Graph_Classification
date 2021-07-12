b8f4355855dee80a6e5325e9f0fea8d95ed36cae
emmaxjean <77707364+emmaxjean@users.noreply.github.com>
2021-07-11 20:04:39 -0500

    Update Ripser_Randomforestclassification_with_dimensionscaling.py
    Modified comments. Left comments on lines 186, 184, and 174 where certain datasets had errors.

--- src/Ripser_Randomforestclassification_with_dimensionscaling.py
+++ src/Ripser_Randomforestclassification_with_dimensionscaling.py
@@ -31,7 +31,7 @@ def standardGraphFile(dataset):
     edgelabels = pd.read_csv(datapath + "/" + dataset + "/" + dataset + "_graph_labels.txt", header=None)
     grapher = sum(graphlabels.values.tolist(), [])
     data = list(set(grapher))  # counting unique graph ids
-   #data = shuffle(graph1)
+    #data = shuffle(graph1)
 
 
     Training_set, Test_set = train_test_split(data, train_size=0.8, test_size=0.2)
@@ -43,16 +43,16 @@ def standardGraphFile(dataset):
         graphEdges = edgedata[edgedata.index.isin(graphNodes)]
         graph = Graph.TupleList(graphEdges.itertuples(index=False), directed=False, weights=True)
         distmat = np.asarray(Graph.shortest_paths_dijkstra(graph))
-      #  fig, ax = plt.subplots()
-       # plot(graph, target = ax)
-       # plt.show()
-       # [mg, Mg] = [np.nanmin(distmat), np.nanmax(distmat[distmat != np.inf])]
+        #  fig, ax = plt.subplots()
+        # plot(graph, target = ax)
+        # plt.show()
+        # [mg, Mg] = [np.nanmin(distmat), np.nanmax(distmat[distmat != np.inf])]
         distmat[distmat == inf] = 0
-       # mds_embedding = MDS(n_components=3, dissimilarity='precomputed').fit_transform(distmat)
-       # tsne_embedding = TSNE(n_components=3).fit_transform(distmat)
-       # pca_embedding = PCA(n_components=3).fit_transform(distmat)
-      #  [mi, ma] = [np.nanmin(tsne_embedding), np.nanmax(tsne_embedding)]
-      #  norm_embedding = tsne_embedding/ma
+        # mds_embedding = MDS(n_components=3, dissimilarity='precomputed').fit_transform(distmat)
+        # tsne_embedding = TSNE(n_components=3).fit_transform(distmat)
+        # pca_embedding = PCA(n_components=3).fit_transform(distmat)
+        #  [mi, ma] = [np.nanmin(tsne_embedding), np.nanmax(tsne_embedding)]
+        #  norm_embedding = tsne_embedding/ma
         [mi, ma] = [np.nanmin(distmat), np.nanmax(distmat)]
         norm_distmat = distmat / ma
         [m, M] = [np.nanmin(norm_distmat), np.nanmax(norm_distmat)]
@@ -90,7 +90,7 @@ def standardGraphFile(dataset):
             BB_2.append(B_2)
 
         Betti_numbers = BB_0 + BB_1 + BB_2 # concatenate betti numbers
-       # Betti_graphid = [Betti_numbers, i]  # save Betti numbers with the graphid
+        # Betti_graphid = [Betti_numbers, i]  # save Betti numbers with the graphid
 
         graphidlocation = data.index(i)  # obtain the locations of the graphid
         graphlabelslocation = (edgelabels.values[graphidlocation]).tolist()  # extract the corresponding graphlabels of the graphid
@@ -104,16 +104,16 @@ def standardGraphFile(dataset):
         testgraphEdges = edgedata[edgedata.index.isin(testgraphNodes)]
         testgraph = Graph.TupleList(testgraphEdges.itertuples(index=False), directed=False, weights=True)
         testdistmat = np.asarray(Graph.shortest_paths_dijkstra(testgraph))
-      #  [ng, Ng] = [np.nanmin(testdistmat), np.nanmax(testdistmat[testdistmat != np.inf])]
+        #  [ng, Ng] = [np.nanmin(testdistmat), np.nanmax(testdistmat[testdistmat != np.inf])]
         testdistmat[testdistmat == inf] = 0
-       # mdstest_embedding = MDS(n_components=3, dissimilarity='precomputed').fit_transform(testdistmat)
-       # tsnetest_embedding = TSNE(n_components=3).fit_transform(testdistmat)
-       # pcatest_embedding = PCA(n_components=3).fit_transform(testdistmat)
-       # [ni, na] = [np.nanmin(tsnetest_embedding), np.nanmax(tsnetest_embedding)]
-       # normtest_embedding = tsnetest_embedding/na
+        # mdstest_embedding = MDS(n_components=3, dissimilarity='precomputed').fit_transform(testdistmat)
+        # tsnetest_embedding = TSNE(n_components=3).fit_transform(testdistmat)
+        # pcatest_embedding = PCA(n_components=3).fit_transform(testdistmat)
+        # [ni, na] = [np.nanmin(tsnetest_embedding), np.nanmax(tsnetest_embedding)]
+        # normtest_embedding = tsnetest_embedding/na
         [ni, na] = [np.nanmin(testdistmat), np.nanmax(testdistmat)]
         normtest_distmat = testdistmat/na
-      #  [n, N] = [np.nanmin(normtest_embedding), np.nanmax(normtest_embedding)]
+        #  [n, N] = [np.nanmin(normtest_embedding), np.nanmax(normtest_embedding)]
         testdiagrams = ripser(normtest_distmat, thresh=0.5, maxdim=2, distance_matrix=True)['dgms']
 
 
@@ -171,7 +171,7 @@ def standardGraphFile(dataset):
 
     RFC = RandomForestClassifier()
     grid = GridSearchCV(estimator=RFC, param_grid=Param_Grid, cv=10, n_jobs=1)
-    grid.fit(Train_features, Train_labels)
+    grid.fit(Train_features, Train_labels) # FIRSTMM_DB dataset doesn't work with this
     param_choose = grid.best_params_
 
     RFC_pred = RandomForestClassifier(**param_choose, random_state=1).fit(Train_features, Train_labels)
@@ -181,9 +181,9 @@ def standardGraphFile(dataset):
     r_probs = [0 for _ in range(len(Test_labels))]  # worst case scenario
     RFC_probs = (RFC_pred.predict_proba(Test_features))[:,1]  # predict the class probabilities for K_test and keep the positive outcomes
     r_auc = roc_auc_score(Test_labels, r_probs) # Compute area under the receiver operating characteristic (ROC) curve for worst case scenario
-    RFC_auc = roc_auc_score(Test_labels, RFC_probs)  # Compute area under the receiver operating characteristic curve for RandomForest
+    RFC_auc = roc_auc_score(Test_labels, RFC_probs)  # Compute area under the receiver operating characteristic curve for RandomForest # problem with ENZYME here
 
-    r_fpr, r_tpr, thresholds = roc_curve(Test_labels, r_probs)
+    r_fpr, r_tpr, thresholds = roc_curve(Test_labels, r_probs) # problem with PROTEINS here
     RFC_fpr, RFC_tpr, thresholds = roc_curve(Test_labels, RFC_probs)  # compute ROC
     #RFAC_auc = auc(RFC_fpr, RFC_tpr) #basically does the same thing as RFC_auc
 
@@ -196,17 +196,17 @@ def standardGraphFile(dataset):
     plt.legend()  # show legend
     plt.show()  # show plot
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
-=======
+#=======
     
     
     tsv_writer.writerow([dataset, '%.3f' % r_auc, '%.3f' % RFC_auc, accuracy_score(Test_labels, Test_pred), time() - start])  # if you want more output you must include here and update column names
 
     
->>>>>>> a2aa5094b0ad84077814ae0f2b2ac10c50bd6023
+#>>>>>>> a2aa5094b0ad84077814ae0f2b2ac10c50bd6023
 if __name__ == '__main__':
-    sets = ['BZR', 'COX2', 'DHFR', 'ENZYMES', 'FIRSTMM_DB', 'FRANKENSTEIN', 'PROTEINS']
+    sets = ['BZR', 'COX2', 'DHFR', 'FRANKENSTEIN', 'PROTEINS', 'ENZYMES', 'FIRSTMM_DB']
     with open('../results/Ripser_RFC_w_dim_scaling_output.tsv', 'wt') as out_file: #opens output file
         tsv_writer = csv.writer(out_file, delimiter='\t') #makes output file into a tsv
         tsv_writer.writerow(['dataset', 'Random_prediction_(AUROC=)', 'RFC_(AUROC=)', 'accuracy_score(Test_labels,test_pred)', 'run_time']) # column names: if you want more output you must create a column name here
