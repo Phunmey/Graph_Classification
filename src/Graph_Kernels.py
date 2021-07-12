f657cfd0831e8976456a8ef74b1d852415c83bef
speters-github <umpet347@myumanitoba.ca>
2021-07-12 13:51:14 -0500

    Split common helper functions off into a separate file "Helper_Functions.py".  Added block comments to sections of code that were before and after the changes made during monday morning's meeting.  Currently it's set so PROTEINS still runs properly.

--- src/Graph_Kernels.py
+++ src/Graph_Kernels.py
@@ -1,22 +1,8 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_auc_score, roc_curve, auc
-from time import time
-from grakel.kernels import WeisfeilerLehman, VertexHistogram, WeisfeilerLehmanOptimalAssignment, ShortestPath
-import csv
-
-"""
-UNUSED IMPORTS:
-    import numpy as np
-    from ripser import ripser
-    from persim import plot_diagrams
-    from pandas import DataFrame
-    from igraph import *
-    from matplotlib.pyplot import figure
-    from plotly import graph_objs as go
-"""
+from sklearn.metrics import accuracy_score, roc_auc_score
+from grakel.kernels import WeisfeilerLehman, VertexHistogram
+from Helper_Functions import *
 
 
 def standard_graph_file(data_set, tsv_file):
@@ -54,15 +40,6 @@ def get_data_and_labels(data_set):
     return data, graph_labels
 
 
-def get_read_csv(data_set, extension):
-    path = "../data"
-    return pd.read_csv(path + "/" + data_set + "/" + data_set + extension, header=None)
-
-
-def get_csv_value_sum(data_set, extension):
-    return sum((get_read_csv(data_set, extension).values.tolist()), [])
-
-
 def perform_rfc(data_set, data, graph_labels, start, tsv_file):
     g_train, g_test, y_train, y_test = train_test_split(data, graph_labels,
                                                         test_size=0.2, random_state=42)
@@ -76,65 +53,45 @@ def perform_rfc(data_set, data, graph_labels, start, tsv_file):
 
     # PREDICTION PROBABILITIES
     r_prob = [0 for _ in range(len(y_test))]  # worst case scenario
+
     # predict the class prob. for k_test and keep the positive outcomes
+
+    """
+    # NEW VERSION from monday meeting
     rfc_prob = (rfc_pred.predict_proba(k_test))#[:, 1]
-    # r_auc = roc_auc_score(y_test, r_prob, multi_class='ovr')
+    r_auc = roc_auc_score(y_test, r_prob, multi_class='ovr')
     rfc_auc = roc_auc_score(y_test, rfc_prob, multi_class='ovr')  # Compute AUROC scores
+    """
+
+    # OLD VERSION from before monday meeting
+    rfc_prob = (rfc_pred.predict_proba(k_test))[:, 1]
+    r_auc = roc_auc_score(y_test, r_prob)
+    rfc_auc = roc_auc_score(y_test, rfc_prob)  # Compute AUROC scores
+
     acc_score = accuracy_score(y_test, y_pred)
 
-    # print('Random prediction: AUROC = %.3f' % r_auc)
+    print('Random prediction: AUROC = %.3f' % r_auc)
     print('RFC: AUROC = %.3f' % rfc_auc)
     print("Accuracy score:", acc_score)
     print(f'Time taken to run: {time() - start} seconds')
 
-    # write_tsv(data_set, r_auc, rfc_auc, acc_score, start, tsv_file)
-    write_tsv(data_set, rfc_auc, acc_score, start, tsv_file)
-    # plot_roc_curve(data_set, y_test, r_prob, rfc_prob, r_auc, rfc_auc)
-    plot_roc_curve(data_set, y_test, r_prob, rfc_prob, rfc_auc)
+    """
+    # NEW VERSION from monday meeting
+    # write_tsv(data_set, rfc_auc, acc_score, start, tsv_file)
+    # plot_roc_curve(data_set, y_test, r_prob, rfc_prob, rfc_auc)
+    """
 
-
-# def write_tsv(data_set, r_auc, rfc_auc, acc_score, start, tsv_file):
-def write_tsv(data_set, rfc_auc, acc_score, start, tsv_file):
-    # if you want more output you must include here and update column names
-    # tsv_file.writerow([data_set, '%.3f' % r_auc, '%.3f' % rfc_auc, acc_score,
-    tsv_file.writerow([data_set, '%.3f' % rfc_auc, acc_score,
-                       time() - start])
-
-
-# def plot_roc_curve(data_set, y_test, r_prob, rfc_prob, r_auc, rfc_auc):
-def plot_roc_curve(data_set, y_test, r_prob, rfc_prob, rfc_auc):
-    # PLOTTING THE ROC_CURVE
-    # r_fpr, r_tpr, thresholds = roc_curve(y_test, r_prob, pos_label=2)
-    # rfc_fpr, rfc_tpr, thresholds = roc_curve(y_test, rfc_prob, pos_label=2)  # compute ROC
-    rfc_fpr, rfc_tpr, thresholds = roc_curve(y_test, rfc_prob)  # compute ROC
-    # rfc_fpr, rfc_tpr, thresholds = roc_curve(y_test, rfc_prob, pos_label=6)  # compute ROC
-    # rfac_auc = auc(rfc_fpr, rfc_tpr)
-
-    plt.figure(figsize=(4, 4), dpi=100)
-    # plt.plot(r_fpr, r_tpr, marker='.', label='Chance prediction (AUROC= %.3f)' % r_auc)
-    plt.plot(rfc_fpr, rfc_tpr, linestyle='-', label='RFC (AUROC= %.3f)' % rfc_auc)
-    plt.title('ROC Plot')  # title
-    plt.xlabel('False Positive Rate')  # x-axis label
-    plt.ylabel('True Positive Rate')  # y-axis label
-    plt.legend()  # show legend
-    plt.savefig("../results/Graph_Kernels/plots/" + data_set + ".png")  # save the plot
-    plt.show()  # show plot
-
-
-def get_tsv_writer(file):
-    tsv_file = csv.writer(file, delimiter='\t')  # makes output file into a tsv
-
-    # column names: if you want more output you must create a column name here
-    tsv_file.writerow(['dataset', 'Random_prediction_(AUROC)', 'RFC_(AUROC)',
-                       'accuracy_score', 'run_time'])
-
-    return tsv_file
+    # OLD VERSION from before monday meeting
+    write_tsv(data_set, r_auc, rfc_auc, acc_score, start, tsv_file)
+    plot_roc_curve(data_set, y_test, r_prob, rfc_prob, r_auc, rfc_auc)
 
 
 if __name__ == '__main__':
     # removed "ENZYMES" and "FIRSTMM_DB" as they both have an error appear
-    # datasets = ["BZR", "COX2", "DHFR", "PROTEINS"]
-    datasets = ["ENZYMES"]
+    # datasets = ["BZR", "COX2", "DHFR", "PROTEINS"]  # initial datasets
+    # datasets = ["DD", "NCI1", "REDDIT-BINARY"]  # new datasets
+    # datasets = ["ENZYMES"]  # run against the file with errors
+    datasets = ["PROTEINS"]  # run against the file that's good
 
     output_file = open("../results/Graph_Kernels/Graph_Kernels_output.tsv", "wt")
     tsv_writer = get_tsv_writer(output_file)
---
+++ src/Helper_Functions.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+from time import time
+from sklearn.metrics import roc_curve
+
+
+def get_read_csv(data_set, extension):
+    path = "../data"
+    return pd.read_csv(path + "/" + data_set + "/" + data_set + extension, header=None)
+
+
+def get_csv_value_sum(data_set, extension):
+    return sum((get_read_csv(data_set, extension).values.tolist()), [])
+
+
+def get_tsv_writer(file):
+    tsv_file = csv.writer(file, delimiter='\t')  # makes output file into a tsv
+
+    # column names: if you want more output you must create a column name here
+    tsv_file.writerow(['dataset', 'Random_prediction_(AUROC)', 'RFC_(AUROC)',
+                       'accuracy_score', 'run_time'])
+
+    return tsv_file
+
+
+# def write_tsv(data_set, rfc_auc, acc_score, start, tsv_file):
+def write_tsv(data_set, r_auc, rfc_auc, acc_score, start, tsv_file):
+    # if you want more output you must include here and update column names
+    """
+    # NEW VERSION from monday meeting
+    tsv_file.writerow([data_set, '%.3f' % rfc_auc, acc_score, time() - start])
+    """
+
+    # OLD VERSION from before monday meeting
+    tsv_file.writerow([data_set, '%.3f' % r_auc, '%.3f' % rfc_auc, acc_score,
+                       time() - start])
+
+
+# def plot_roc_curve(data_set, y_test, r_prob, rfc_prob, rfc_auc):
+def plot_roc_curve(data_set, y_test, r_prob, rfc_prob, r_auc, rfc_auc):
+    # PLOTTING THE ROC_CURVE
+    """
+    # NEW VERSION from monday meeting
+    # rfc_fpr, rfc_tpr, thresholds = roc_curve(y_test, rfc_prob)  # compute ROC
+    # rfc_fpr, rfc_tpr, thresholds = roc_curve(y_test, rfc_prob, pos_label=6)  # compute ROC
+    """
+
+    # OLD VERSION from before monday meeting
+    r_fpr, r_tpr, thresholds = roc_curve(y_test, r_prob, pos_label=2)
+    rfc_fpr, rfc_tpr, thresholds = roc_curve(y_test, rfc_prob, pos_label=2)  # compute ROC
+
+    # rfac_auc = auc(rfc_fpr, rfc_tpr)
+
+    plt.figure(figsize=(4, 4), dpi=100)
+    plt.plot(r_fpr, r_tpr, marker='.', label='Chance prediction (AUROC= %.3f)' % r_auc)
+    plt.plot(rfc_fpr, rfc_tpr, linestyle='-', label='RFC (AUROC= %.3f)' % rfc_auc)
+    plt.title('ROC Plot')  # title
+    plt.xlabel('False Positive Rate')  # x-axis label
+    plt.ylabel('True Positive Rate')  # y-axis label
+    plt.legend()  # show legend
+    plt.savefig("../results/Graph_Kernels/plots/" + data_set + ".png")  # save the plot
+    plt.show()  # show plot
